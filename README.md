56646eb601e34fa6d1737b07443da24120a55c81
Fülöp Tibor <fuloptibi03@gmail.com>
2022-04-14 20:55:15 +0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,76 @@
+<div id="top"></div>
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/TibixDev/MPVPlay">
+    <img src="https://i.imgur.com/2t1lkUp.png" alt="MPVPlay Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">MPVPlay</h3>
+
+  <p align="center">
+    MPVPlay is a combination of a `.desktop` file and a userscript that allows you to play YouTube videos directly in MPV with the click of a button. The UI integrates nicely into the YouTube button bar where the likes/dislike buttons reside aswell.
+    <br />
+    <br />
+    <a href="https://github.com/TibixDev/MPVPlay/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/TibixDev/MPVPlay/issues">Request Feature</a>
+  </p>
+</div>
+
+## Getting Started
+
+### Dependencies
+* Linux
+* A Chromium or Firefox based browser that supports custom protocols
+* TamperMonkey or GreaseMonkey extension
+
+### Installing & Running
+**Step 1 - `.desktop` installation**
+```
+git clone https://github.com/TibixDev/MPVPlay
+cd MPVPlay
+cp mpv-opener.desktop ~/.local/share/applications/
+cd ~/.local/share/applications/
+xdg-mime default mpv-opener.desktop x-scheme-handler/mpv
+update-desktop-database
+```
+
+**Step 2 - Userscript installation**
+
+1. Downloading the [ViolentMonkey](https://violentmonkey.github.io/get-it/) extension for your browser.
+2. Click the extension icon, click the + button.
+3. Paste in the contents of `userscript/mpv.js`, save (CTRL + S).
+4. Enjoy! Now when you visit any YouTube video, you will see a button in the like/dislike bar to play the video with MPV.
+
+
+
+## Roadmap
+* Windows Support
+* Whatever the community wants
+
+## Help
+If you encounter any issues feel free to open an issue.
+
+## Contribution
+If you feel like this project could use some improvements feel free to open a PR. I'll make sure to review it. :)
+
+## Authors
+[@TibixDev](https://github.com/TibixDev)
+
+## Version History
+* 0.1
+    * Initial Release
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+Special thanks, inspiration, code snippets, etc.
+* [Levev](https://github.com/Levev) - Massive thanks to you for helping me with the bash!
+* [StackOverflow - Creating URL schemes in Linux](https://unix.stackexchange.com/a/497147)
+* [StackOverflow - Escaping single-quotes in bash](https://stackoverflow.com/a/1250279/10771609)
+* [StackOverflow - Replacing stuff in bash](https://stackoverflow.com/a/13210909/10771609)
---
+++ userscript/mpv.js
@@ -0,0 +1,91 @@
+// ==UserScript==
+// @name         YouTube MPV Player
+// @version      0.1
+// @description  This little script opens any YouTube video in MPV with a simple button click
+// @author       TibixDev
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(async function () {
+    'use strict';
+    console.log("[YTMPV] YouTube MPV player script loaded");
+
+    function setButtonInterval() {
+        return setInterval(() => {
+            if (document.querySelector("#info>#menu-container>#menu>ytd-menu-renderer>#top-level-buttons-computed>ytd-toggle-button-renderer")) {
+                console.log("[YTMPV] Menu container found, executing...");
+                addMpvButton()
+            }
+        }, 1000);
+    }
+
+    // let waitForButtons = setButtonInterval();
+    let waitForButtons = null;
+
+    let location = window.location.href;;
+    if (location.match(/^https:\/\/www\.youtube\.com\/watch\?v=([^&]*)/)) {
+        waitForButtons = setButtonInterval();
+    }
+
+    let waitForUrlChange = setInterval(() => {
+        if (location !== window.location.href && window.location.href.includes("watch?v=") && !waitForButtons) {
+            console.log("[YTMPV] Video URL detected, toggling waitForButtons...");
+            waitForButtons = setButtonInterval();
+            location = window.location.href;
+        }
+    }, 2000);
+
+    function addMpvButton() {
+        clearInterval(waitForButtons);
+        waitForButtons = null;
+        const ytButtons = document.querySelector("#info>#menu-container>#menu>ytd-menu-renderer>#top-level-buttons-computed");
+        const ytButton = document.createElement("button");
+        ytButton.id = "mpv-button";
+        const mpvBtnStyle = `
+        #mpv-button {
+            color: white;
+            cursor: pointer;
+            background-color: #043565;
+            border-radius: 10px;
+            margin-left: 10px;
+            margin-right: 10px;
+            padding-left: 10px;
+            padding-right: 10px;
+            font-size: var(--ytd-tab-system_-_font-size);
+            font-weight: var(--ytd-tab-system_-_font-weight);
+            font-family: Roboto, Arial, sans-serif;
+            border: 0;
+            transition: all 0.2s ease-in-out;
+        }
+    
+        #mpv-button:hover {
+            background-color: #0a6dab;
+        }`
+    
+        const styleElem = document.createElement("style");
+        if (styleElem.styleSheet) {
+            styleElem.styleSheet.cssText = mpvBtnStyle;
+        } else {
+            styleElem.appendChild(document.createTextNode(mpvBtnStyle));
+        }
+        document.getElementsByTagName('head')[0].appendChild(styleElem);
+        // ytButton.style.cssText = mpvBtnStyle;
+        ytButton.textContent = "▶ MPV";
+        ytButton.addEventListener("click", () => {
+            document.querySelector("video").pause();
+            document.location = "mpv://" + document.location.href;
+            ytButton.textContent = "⌛ Opening...";
+            ytButton.style.cssText = "background-color: #0a6dab;";
+            setTimeout(() => {
+                ytButton.textContent = "▶ MPV";
+                ytButton.style.cssText = "";
+            }, 3000);
+        });
+    
+        ytButtons.appendChild(ytButton);
+        console.log("[YTMPV] MPV button added");
+        console.log(ytButton, ytButtons);
+    }
+})();
